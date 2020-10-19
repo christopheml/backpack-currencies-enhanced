92a8140834ffee161b21a92ab3f71c913fa91a38
Christophe Michel <christopheml@users.noreply.github.com>
2020-10-19 18:17:59 +0200

    Fixed the zip packaging

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -u
 set -o pipefail
 
 version=`cat BackpackCurrenciesEnhanced.toc | grep Version | cut -f2 -d: | xargs`
-directory="BackpackCurrenciesEnhanced-${version}"
-mkdir -p ${directory}
+directory="BackpackCurrenciesEnhanced"
+archive="${directory}-${version}.zip"
+mkdir -p "${directory}/"
 cp BackpackCurrenciesEnhanced.toc BackpackCurrenciesEnhanced.lua "${directory}/"
-zip -m -9 -r ${directory}.zip "${directory}/"
+zip -m -9 -r "${archive}" "${directory}/"
